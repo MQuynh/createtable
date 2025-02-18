03057b71ea5b2595c791d71d8e867a711cce594a
Hopdc76 <168882726+Hopdc76@users.noreply.github.com>
2025-02-18 13:09:52 +0700

    Update createtable.py

--- createtable.py
+++ createtable.py
@@ -145,15 +145,15 @@ with tab1:
     **Ví dụ:**
     - Ô "Tên cột":
         ```
-        Họ và tên
-        Ngày sinh
-        Điểm trung bình
+        Ngân hàng
+        Ngày giao dịch
+        Số tiền
         ```
     - Ô "Giá trị mẫu":
         ```
-        Nguyễn Văn A
-        01/01/2000
-        8.5
+        ACB
+        01/01/2025
+        8000
         ```
     """)
 
@@ -205,7 +205,7 @@ with tab2:
     **Ví dụ:**
     | Tên cột         | Giá trị mẫu   |
     |------------------|---------------|
-    | Họ và tên       | Nguyễn Văn A  |
-    | Ngày sinh       | 01/01/2000    |
-    | Điểm trung bình | 8.5           |
+    | Ngân hàng       | ACB  |
+    | Ngày giao dịch       | 01/01/2025    |
+    | Số tiền | 1000           |
     """)
